d3f2ace3981f39abb04b344ac89cfc357977a893
Phil Bayfield <phil@bayfmail.com>
2014-05-04 00:49:46 +0100

    create directory before trying to copy files

--- manifests/files.pp
+++ manifests/files.pp
@@ -6,11 +6,19 @@ define openvpn_client::files (
   Openvpn::Client[$name] ->
   Openvpn_client::Files[$name]
   
-  file { "/etc/puppet/files/openvpn/${server}/download-configs/${name}/keys/ca.crt":
-    owner => puppet,
-    group => puppet,
-    mode => 400,
-    source => "/etc/openvpn/${server}/download-configs/${name}/keys/ca.crt"
+  file {
+    "/etc/puppet/files/openvpn/${server}/download-configs/${name}/keys":
+	    ensure => "directory",
+	    owner => 'puppet',
+	    group => 'puppet',
+	    mode => 400;
+	    
+    "/etc/puppet/files/openvpn/${server}/download-configs/${name}/keys/ca.crt":
+	    owner => 'puppet',
+	    group => 'puppet',
+	    mode => 400,
+	    source => "/etc/openvpn/${server}/download-configs/${name}/keys/ca.crt",
+	    require => File[ "/etc/puppet/files/openvpn/${server}/download-configs/${name}/keys" ];
   }
   
 }
