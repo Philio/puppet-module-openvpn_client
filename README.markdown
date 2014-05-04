cab1194ec07838c143a7ec90b3e45151bbe859d2
Phil Bayfield <phil@bayfmail.com>
2014-05-04 16:09:18 +0100

    add some basic documentation

--- README.markdown
+++ README.markdown
@@ -1,3 +1,20 @@
 # openvpn_client #
 
-This is the openvpn_client module. It provides...
+This module works with luxflux/openvpn to automate VPN client deployment to
+servers managed by puppet. It achieves this by copying configs created by
+luxflux/openvpn to the puppet file server and which are then pulled to clients
+and installed.
+
+Sample Usage:
+
+* Copying configuration files to the file server on the puppet master node:
+
+    openvpn_client::files { 'my_client':
+      server => 'openvpn',
+    }
+
+* Installing and starting the OpenVPN client on a configured node:
+
+    openvpn_client::client { 'my_client:
+      server => 'openvpn',
+    }
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,14 +1,27 @@
 # Class: openvpn_client
 #
-# This module manages openvpn_client
+# This module works with luxflux/openvpn to automate VPN client deployment to
+# servers managed by puppet. It achieves this by copying configs created by
+# luxflux/openvpn to the puppet file server and which are then pulled to clients
+# and installed.
 #
 # Parameters: none
-#
-# Actions:
 #
 # Requires: see Modulefile
 #
 # Sample Usage:
+#
+# * Copying configuration files to the file server on the puppet master node:
+#
+#     openvpn_client::files { 'my_client':
+#       server => 'openvpn',
+#     }
+#
+# * Installing and starting the OpenVPN client on a configured node:
+#
+#     openvpn_client::client { 'my_client:
+#       server => 'openvpn',
+#     }
 #
 class openvpn_client {
 
