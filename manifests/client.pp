ac429a251af6b636560330fb8451113223de9e98
Phil Bayfield <phil@bayfmail.com>
2014-05-23 10:31:27 +0100

    client should export resources

--- manifests/client.pp
+++ manifests/client.pp
@@ -7,34 +7,36 @@ define openvpn_client::client (
   Openvpn_client::Client[$name] ~>
   Class['openvpn_client::service']
   
-  file {
-    "/etc/openvpn/${name}.conf":
-      ensure  => file,
-      owner   => 'root',
-      group   => 'root',
-      mode    => 644,
-      source  => "puppet://management.codacity.net/files/openvpn/${server}/download-configs/${name}/${name}.conf";
-
-    "/etc/openvpn/keys/ca.crt":
-      ensure  => file,
-      owner   => 'root',
-      group   => 'root',
-      mode    => 644,
-      source  => "puppet://management.codacity.net/files/openvpn/${server}/download-configs/${name}/keys/ca.crt";
-      
-    "/etc/openvpn/keys/${name}.crt":
-      ensure  => file,
-      owner   => 'root',
-      group   => 'root',
-      mode    => 644,
-      source  => "puppet://management.codacity.net/files/openvpn/${server}/download-configs/${name}/keys/${name}.crt";
-      
-    "/etc/openvpn/keys/${name}.key":
-      ensure  => file,
-      owner   => 'root',
-      group   => 'root',
-      mode    => 600,
-      source  => "puppet://management.codacity.net/files/openvpn/${server}/download-configs/${name}/keys/${name}.key";
-  }
+  File <<| tag == "${server}-${name}" |>>
+  
+#  file {
+#    "/etc/openvpn/${name}.conf":
+#      ensure  => file,
+#      owner   => 'root',
+#      group   => 'root',
+#      mode    => 644,
+#      source  => "puppet://management.codacity.net/files/openvpn/${server}/download-configs/${name}/${name}.conf";
+#
+#    "/etc/openvpn/keys/ca.crt":
+#      ensure  => file,
+#      owner   => 'root',
+#      group   => 'root',
+#      mode    => 644,
+#      source  => "puppet://management.codacity.net/files/openvpn/${server}/download-configs/${name}/keys/ca.crt";
+#      
+#    "/etc/openvpn/keys/${name}.crt":
+#      ensure  => file,
+#      owner   => 'root',
+#      group   => 'root',
+#      mode    => 644,
+#      source  => "puppet://management.codacity.net/files/openvpn/${server}/download-configs/${name}/keys/${name}.crt";
+#      
+#    "/etc/openvpn/keys/${name}.key":
+#      ensure  => file,
+#      owner   => 'root',
+#      group   => 'root',
+#      mode    => 600,
+#      source  => "puppet://management.codacity.net/files/openvpn/${server}/download-configs/${name}/keys/${name}.key";
+#  }
   
 }
