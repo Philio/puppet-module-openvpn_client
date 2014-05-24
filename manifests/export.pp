c01f35d40ceda1210ae1c58a4539ad82fdc86fbe
Phil Bayfield <phil@bayfmail.com>
2014-05-24 19:37:59 +0100

    test adding data from fact

--- manifests/export.pp
+++ manifests/export.pp
@@ -12,15 +12,15 @@ define openvpn_client::export (
     fail ('openvpn_cert_data not defined, is pluginsync enabled?')
   }
   
-#  @@file { "/etc/openvpn/${name}.conf":
-#    ensure => file,
-#    owner => 'root',
-#    group => 'root',
-#    mode => 644,
-#    content  => file("/etc/openvpn/${server}/download-configs/${name}/${name}.conf"),
-#    tag => "${server}-${name}",
-#  }
-#  
+  @@file { "/etc/openvpn/${name}.conf":
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => 644,
+    content => $data["${server}-${name}-conf"],
+    tag => "${server}-${name}",
+  }
+  
 #  @@file { "/etc/openvpn/keys/ca.crt":
 #    ensure  => file,
 #    owner   => 'root',
