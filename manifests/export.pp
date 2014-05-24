ad4f08feee2ec695ec711f6cff47c85d115aafee
Phil Bayfield <phil@bayfmail.com>
2014-05-24 19:18:48 +0100

    add custom fact for cert data

--- manifests/export.pp
+++ manifests/export.pp
@@ -6,13 +6,40 @@ define openvpn_client::export (
   Openvpn::Client[$name] ->
   Openvpn_client::Export[$name]
   
-  @@file { "/etc/openvpn/${name}.tar.gz":
-    ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => 644,
-    content  => template("/etc/openvpn/${server}/download-configs/${name}.tar.gz"),
-    tag => "${server}-${name}",
-  }
+#  @@file { "/etc/openvpn/${name}.conf":
+#    ensure => file,
+#    owner => 'root',
+#    group => 'root',
+#    mode => 644,
+#    content  => file("/etc/openvpn/${server}/download-configs/${name}/${name}.conf"),
+#    tag => "${server}-${name}",
+#  }
+#  
+#  @@file { "/etc/openvpn/keys/ca.crt":
+#    ensure  => file,
+#    owner   => 'root',
+#    group   => 'root',
+#    mode    => 644,
+#    content  => file("/etc/openvpn/${server}/easy-rsa/keys/ca.crt"),
+#    tag => "${server}-${name}",
+#  }
+#  
+#  @@file { "/etc/openvpn/keys/${name}.crt":
+#    ensure  => file,
+#    owner   => 'root',
+#    group   => 'root',
+#    mode    => 644,
+#    content => file("/etc/openvpn/${server}/easy-rsa/keys/${name}.crt"),
+#    tag => "${server}-${name}",
+#  }
+#
+#  @@file { "/etc/openvpn/keys/${name}.key":
+#    ensure  => file,
+#    owner   => 'root',
+#    group   => 'root',
+#    mode    => 600,
+#    content => file("/etc/openvpn/${server}/easy-rsa/keys/${name}.key"),
+#    tag => "${server}-${name}",
+#  }
 
 }
