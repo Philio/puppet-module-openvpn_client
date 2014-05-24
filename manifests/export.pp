f511b69c9525a34fb849d00c2cbaab187c7d72ba
Phil Bayfield <phil@bayfmail.com>
2014-05-24 21:25:57 +0100

    update resource names to avoid collisions when mulitple clients defined

--- manifests/export.pp
+++ manifests/export.pp
@@ -12,7 +12,8 @@ define openvpn_client::export (
     fail ('openvpn_cert_data not defined, is pluginsync enabled?')
   }
   
-  @@file { "/etc/openvpn/${name}.conf":
+  @@file { "${server}-${name}-config":
+    path => "/etc/openvpn/${name}.conf",
     ensure => file,
     owner => 'root',
     group => 'root',
@@ -21,29 +22,32 @@ define openvpn_client::export (
     tag => "${server}-${name}",
   }
   
-  @@file { "/etc/openvpn/keys/ca.crt":
-    ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => 644,
-    content  => $data["${server}-${name}-ca"],
+  @@file { "${server}-${name}-ca":
+    path => "/etc/openvpn/keys/ca.crt",
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => 644,
+    content => $data["${server}-${name}-ca"],
     tag => "${server}-${name}",
   }
   
-  @@file { "/etc/openvpn/keys/${name}.crt":
-    ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => 644,
+  @@file { "${server}-${name}-crt":
+    path => "/etc/openvpn/keys/${name}.crt",
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => 644,
     content => $data["${server}-${name}-crt"],
     tag => "${server}-${name}",
   }
 
-  @@file { "/etc/openvpn/keys/${name}.key":
-    ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => 600,
+  @@file { "${server}-${name}-key":
+    path => "/etc/openvpn/keys/${name}.key",
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => 600,
     content => $data["${server}-${name}-key"],
     tag => "${server}-${name}",
   }
